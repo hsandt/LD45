e1dcc9199637c96ae2b28cbe266ea548a643bb58
hsandt <hs@fuwar.in>
2019-10-05 21:46:00 +0100

    [DEBUG] Put debug features inside debug symbol conditions to make release build work (as in pico-sonic)

--- src/application/wit_fight_app.lua
+++ src/application/wit_fight_app.lua
@@ -3,12 +3,27 @@
 
 local gameapp = require("engine/application/gameapp")
 require("engine/core/class")
-local codetuner = require("engine/debug/codetuner")
-local logging = require("engine/debug/logging")
-local profiler = require("engine/debug/profiler")
-local vlogger = require("engine/debug/visual_logger")
 local input = require("engine/input/input")
+
+--#if tuner
+local codetuner = require("engine/debug/codetuner")
+--#endif
+
+--#if log
+local logging = require("engine/debug/logging")
+--#endif
+
+--#if profiler
+local profiler = require("engine/debug/profiler")
+--#endif
+
+--#if visual_logger
+local vlogger = require("engine/debug/visual_logger")
+--#endif
+
+--#if mouse
 local ui = require("engine/ui/ui")
+--#endif
 
 local main_menu = require("menu/main_menu")
 local wit_fight = require("fight/wit_fight")
@@ -24,28 +39,50 @@ function wit_fight_app.on_pre_start() -- override
 end
 
 function wit_fight_app.on_post_start() -- override
+--#if mouse
   -- enable mouse devkit
   input:toggle_mouse(true)
   ui:set_cursor_sprite_data(visual_data.sprites.cursor)
+--#endif
 end
 
 function wit_fight_app.on_reset() -- override
+--#if mouse
   ui:set_cursor_sprite_data(nil)
+--#endif
 end
 
 function wit_fight_app.on_update() -- override
+--#if profiler
   profiler.window:update()
+--#endif
+
+--#if visual_logger
   vlogger.window:update()
+--#endif
+
+--#if tuner
   codetuner:update_window()
+--#endif
 end
 
 function wit_fight_app:on_render() -- override
+--#if profiler
   profiler.window:render()
-  vlogger.window:render()
-  codetuner:render_window()
+--#endif
 
+--#if visual_logger
+  vlogger.window:render()
+--#endif
+
+--#if tuner
+  codetuner:render_window()
+--#endif
+
+--#if mouse
   -- always draw cursor on top
   ui:render_mouse()
+--#endif
 end
 
 return wit_fight_app
