976cd74de645b2eb97cd86dd2903c0cc997794f8
hsandt <hs@fuwar.in>
2019-10-05 21:46:18 +0100

    [FIGHT] Draw background: floor and wall

--- src/fight/wit_fight.lua
+++ src/fight/wit_fight.lua
@@ -16,6 +16,28 @@ function wit_fight:update()
 end
 
 function wit_fight:render()
+  self:draw_background()
+  self:draw_characters()
+  self:draw_hud()
+end
+
+function wit_fight:draw_background()
+  -- wall
+  rectfill(0, 0, 127, 127, colors.dark_gray)
+  -- floor
+  rectfill(0, 51, 103, 127, colors.light_gray)
+  line(0, 50, 104, 50, colors.black)
+  line(104, 50, 104, 127, colors.black)
+  -- upper stairs
+  -- lower stairs
+end
+
+function wit_fight:draw_characters()
+
+end
+
+function wit_fight:draw_hud()
+
 end
 
 return wit_fight
