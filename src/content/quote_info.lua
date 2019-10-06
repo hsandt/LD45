09302d5b7d0388e0b4376ff124241c8dda018bd6
hsandt <hs@fuwar.in>
2019-10-06 14:28:59 +0100

    [QUOTE] Added level

--- src/content/quote_info.lua
+++ src/content/quote_info.lua
@@ -7,16 +7,18 @@ local quote_info = new_struct()
 
 -- id: int
 -- quote_type: quote_types
+-- level: int
 -- text: string
-function quote_info:_init(id, quote_type, text)
+function quote_info:_init(id, quote_type, level, text)
   self.id = id
+  self.level = level
   self.quote_type = quote_type
   self.text = text
 end
 
 --#if log
 function quote_info:_tostring()
-  return "quote_info("..joinstr(", ", self.id, self.quote_type, dump(self.text))..")"
+  return "quote_info("..joinstr(", ", self.id, self.quote_type, self.level, dump(self.text))..")"
 end
 --#endif
 
