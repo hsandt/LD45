b2272332fc147582a0c4ed8ef6adc73cc9f0eb54
hsandt <hs@fuwar.in>
2019-10-06 14:18:34 +0100

    [NPC] Added initial_quote_ids

--- src/content/npc_info.lua
+++ src/content/npc_info.lua
@@ -1,14 +1,19 @@
 local npc_info = new_struct()
 
-function npc_info:_init(id, name, level)
+-- id: int
+-- name: string
+-- level: int
+-- initial_quote_ids: {int}
+function npc_info:_init(id, name, level, initial_quote_ids)
   self.id = id
   self.name = name
   self.level = level
+  self.initial_quote_ids = initial_quote_ids
 end
 
 --#if log
 function npc_info:_tostring()
-  return "npc_info("..joinstr(", ", self.id, dump(self.name), self.level)..")"
+  return "npc_info("..joinstr(", ", self.id, dump(self.name), self.level, dump_sequence(self.initial_quote_ids))..")"
 end
 --#endif
 
