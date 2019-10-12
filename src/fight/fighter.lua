2e24dad7a31c4dbfbe531624f8b575ffeb9a7150
hsandt <hs@fuwar.in>
2019-10-12 19:18:26 +0100

    [FIGHTER] Added take_damage and is_alive

--- src/fight/fighter.lua
+++ src/fight/fighter.lua
@@ -4,24 +4,62 @@ local ui = require("engine/ui/ui")
 
 local visual_data = require("resources/visual_data")
 
+control_types = enum {
+  'human',
+  'ai'
+}
+
 local fighter = new_class()
 
--- max_hp: int                initial hp
--- sprite: sprite_data        sprite to render
--- pos: vector                position on screen
--- direction: horizontal_dirs facing left or right?
-function fighter:_init(max_hp, sprite, pos, direction)
-  self.hp = max_hp
+--[[
+Parameters
+  control_type: control_types   is the fighter controlled by the player or some ai?
+  max_hp: int                   initial hp
+  sprite: sprite_data           sprite to render
+  pos: vector                   position on screen
+  direction: horizontal_dirs    facing left or right?
+
+State
+  hp: int                       current hp
+  is_attacker: bool             true iff fighter attacks this turn
+  last_quote: (quote_info|nil)  last quote said, if any
+--]]
+function fighter:_init(control_type, max_hp, sprite, pos, direction)
+  self.control_type = control_type
   self.sprite = sprite
   self.pos = pos
   self.direction = direction
+
+  self.hp = max_hp
+  self.is_attacker = false
+  self.last_quote = nil
 end
 
 --#if log
 function fighter:_tostring()
-  return "fighter("..joinstr(", ", self.hp, self.sprite, self.pos, self.direction)..")"
+  return "fighter("..joinstr(", ", self.control_type, self.hp, self.sprite, self.pos, self.direction)..")"
 end
 --#endif
+
+-- logic
+
+function fighter:auto_pick_quote()
+  assert(self.control_type == control_types.ai)
+  -- todo
+end
+
+function fighter:take_damage(damage)
+  self.hp = self.hp - damage
+  if self.hp <= 0 then
+    self.hp = 0
+  end
+end
+
+function fighter:is_alive()
+  return self.hp > 0
+end
+
+-- render
 
 function fighter:draw()
   self.sprite:render(self.pos)
--- src/fight/fighter_utest.lua
+++ src/fight/fighter_utest.lua
@@ -6,26 +6,60 @@ local sprite_data = require("engine/render/sprite_data")
 
 describe('fighter', function ()
 
+  local mock_sprite = sprite_data(sprite_id_location(1, 3))
+
+  local f
+
+  before_each(function ()
+    f = fighter(control_types.ai, 100, mock_sprite, vector(20, 40), horizontal_dirs.right)
+  end)
+
   describe('_init', function ()
     it('should init a fighter', function ()
-      local mock_sprite = sprite_data(sprite_id_location(1, 3))
-      local f = fighter(100, mock_sprite, vector(20, 40), horizontal_dirs.right)
-      assert.are_same({100, mock_sprite, vector(20, 40), horizontal_dirs.right}, {f.hp, f.sprite, f.pos, f.direction})
+      assert.are_same({control_types.ai, 100, mock_sprite, vector(20, 40), horizontal_dirs.right}, {f.control_type, f.hp, f.sprite, f.pos, f.direction})
     end)
   end)
 
   describe('_tostring', function ()
-    it('fighter(100) => "fighter(100)"', function ()
-      local mock_sprite = sprite_data(sprite_id_location(1, 3))
-      local f = fighter(100, mock_sprite, vector(20, 40), horizontal_dirs.right)
-      assert.are_equal("fighter(100, sprite_data(sprite_id_location(1, 3), tile_vector(1, 1), vector(0, 0), 0), vector(20, 40), 2)", f:_tostring())
+    it('fighter(...) => "fighter(...)"', function ()
+      assert.are_equal("fighter(2, 100, sprite_data(sprite_id_location(1, 3), tile_vector(1, 1), vector(0, 0), 0), vector(20, 40), 2)", f:_tostring())
     end)
   end)
 
+  -- logic
+
+  describe('take_damage', function ()
+
+    it('should reduce the fighter hp', function ()
+      f:take_damage(5)
+      assert.are_equal(95, f.hp)
+    end)
+
+    it('should clamp the reduced hp at 0', function ()
+      f:take_damage(200)
+      assert.are_equal(0, f.hp)
+    end)
+
+  end)
+
+  describe('is_alive', function ()
+
+    it('should return true if hp > 0', function ()
+      f.hp = 1
+      assert.is_true(f:is_alive())
+    end)
+
+    it('should return true if hp <= 0', function ()
+      f.hp = 0
+      assert.is_false(f:is_alive())
+    end)
+
+  end)
+
+  -- render
+
   describe('draw', function ()
     it('should not error', function ()
-      local mock_sprite = sprite_data(sprite_id_location(1, 3))
-      local f = fighter(100, mock_sprite, vector(20, 40), horizontal_dirs.right)
       assert.has_no_errors(function ()
         f:draw()
       end)
