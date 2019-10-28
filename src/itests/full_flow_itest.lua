e06e703e5d21305227bf00893dc62e1d4b9bd145
hsandt <hs@fuwar.in>
2019-10-28 20:35:19 +0000

    [ITEST] Added full flow itest

---
+++ src/itests/full_flow_itest.lua
@@ -0,0 +1,101 @@
+local integrationtest = require("engine/test/integrationtest")
+local itest_manager = integrationtest.itest_manager
+local input = require("engine/input/input")
+local flow = require("engine/application/flow")
+
+local function short_press(button_id)
+  act(function ()
+    input.simulated_buttons_down[0][button_id] = true
+  end)
+  wait(1, true)
+  act(function ()
+    input.simulated_buttons_down[0][button_id] = false
+  end)
+  wait(1, true)
+end
+
+itest_manager:register_itest('#solo start -> first 2 fights',
+    -- keep active_gamestate for now, for retrocompatibility with pico-sonic...
+    -- but without gamestate_proxy, not used
+    {':fight'}, function ()
+
+  -- enter title menu
+  setup_callback(function (app)
+    flow:change_gamestate_by_type(':main_menu')
+  end)
+
+  -- start game
+  short_press(button_ids.o)
+
+  -- skip pc monologue
+  short_press(button_ids.o)
+  wait(2.0)
+  short_press(button_ids.o)
+  wait(2.0)
+  short_press(button_ids.o)
+  wait(2.0)
+  short_press(button_ids.o)
+  wait(2.0)
+  short_press(button_ids.o)
+  wait(2.0)
+  short_press(button_ids.o)
+  wait(2.0)
+  short_press(button_ids.o)
+  wait(2.0)
+  short_press(button_ids.o)
+  wait(2.0)
+  short_press(button_ids.o)
+  wait(2.0)
+  short_press(button_ids.o)
+  wait(2.0)
+  short_press(button_ids.o)
+  wait(2.0)
+  short_press(button_ids.o)
+  wait(2.0)
+  short_press(button_ids.o)
+  wait(2.0)
+  short_press(button_ids.o)
+  wait(2.0)
+  short_press(button_ids.o)
+  wait(2.0)
+  short_press(button_ids.o)
+  wait(2.0)
+  short_press(button_ids.o)
+  wait(2.0)
+
+  -- start 1st fight
+  short_press(button_ids.o)
+  wait(2.0)
+  short_press(button_ids.o)
+  wait(2.0)
+  short_press(button_ids.o)
+  wait(2.0)
+  short_press(button_ids.o)
+  wait(2.0)
+  short_press(button_ids.o)
+  wait(2.0)
+  short_press(button_ids.o)
+  wait(2.0)
+  short_press(button_ids.o)
+  wait(2.0)
+  short_press(button_ids.o)
+  wait(2.0)
+  short_press(button_ids.o)
+  wait(2.0)
+  short_press(button_ids.o)
+  wait(2.0)
+  short_press(button_ids.o)
+  wait(2.0)
+  short_press(button_ids.o)
+  wait(2.0)
+  short_press(button_ids.o)
+  wait(2.0)
+  short_press(button_ids.o)
+  wait(2.0)
+
+
+  final_assert(function ()
+    return flow.curr_state.type == ':fight', "current game state is not ':fight', has instead type: '"..flow.curr_state.type.."'"
+  end)
+
+end)
