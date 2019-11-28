7538e86e7f86128a85f5a1d0ce8624261298b1a6
hsandt <hs@fuwar.in>
2019-11-28 23:24:01 +0000

    [DEBUG] print_data also prints quote power

--- src/debug/print_data.lua
+++ src/debug/print_data.lua
@@ -10,38 +10,47 @@ require("engine/test/pico8api")
 local quote_info = require("content/quote_info")  -- quote_types
 local gameplay_data = require("resources/gameplay_data")
 
-local function get_matching_replies(attack_info)
-  -- code similar to auto_pick_reply
-  local matching_replies = filter(gameplay_data.replies, function (reply)
-    local reply_info = gameplay_data:get_quote(quote_types.reply, reply.id)
-    local quote_match = gameplay_data:get_quote_match(attack_info, reply_info)
-    return quote_match ~= nil  -- power = 0 (cancel reply) is a valid candidate
-  end)
-  return matching_replies
+-- return sequence of all quote matches referring to attack
+local function get_matching_quotes_for_attack(attack_info)
+  local matching_quotes_for_reply = {}
+  for quote_match in all(gameplay_data.quote_matches) do
+     if quote_match.attack_id == attack_info.id then
+       add(matching_quotes_for_reply, quote_match)
+    end
+  end
+  return matching_quotes_for_reply
 end
 
-local function get_matching_attacks(reply_info)
-  -- code similar to auto_pick_reply
-  local matching_replies = filter(gameplay_data.attacks, function (attack)
-    local attack_info = gameplay_data:get_quote(quote_types.attack, attack.id)
-    local quote_match = gameplay_data:get_quote_match(attack_info, reply_info)
-    return quote_match ~= nil  -- power = 0 (cancel reply) is a valid candidate
-  end)
-  return matching_replies
+local function get_matching_quotes_for_reply(reply_info)
+  local matching_quotes_for_attack = {}
+  for quote_match in all(gameplay_data.quote_matches) do
+     if quote_match.reply_id == reply_info.id then
+       add(matching_quotes_for_attack, quote_match)
+    end
+  end
+  return matching_quotes_for_attack
 end
 
 local function print_attack_and_counters_of(attack_id)
   local attack_info = gameplay_data:get_quote(quote_types.attack, attack_id)
   printh(stringify(attack_info))
   printh("=>")
-  printh(joinstr_table('\n', get_matching_replies(attack_info)))
+  local matching_quotes_for_attack = get_matching_quotes_for_attack(attack_info)
+  for matching_quote_for_attack in all(matching_quotes_for_attack) do
+    local reply_info = gameplay_data:get_quote(quote_types.reply, matching_quote_for_attack.reply_id)
+    printh(reply_info.." (power: "..matching_quote_for_attack.power..")")
+  end
 end
 
 local function print_reply_and_attacks_countered_by(reply_id)
   local reply_info = gameplay_data:get_quote(quote_types.reply, reply_id)
   printh(stringify(reply_info))
-  printh("<=")
-  printh(joinstr_table('\n', get_matching_attacks(reply_info)))
+  printh("=>")
+  local matching_quotes_for_reply = get_matching_quotes_for_reply(reply_info)
+  for matching_quote_for_reply in all(matching_quotes_for_reply) do
+    local attack_info = gameplay_data:get_quote(quote_types.attack, matching_quote_for_reply.attack_id)
+    printh(attack_info.." (power: "..matching_quote_for_reply.power..")")
+  end
 end
 
 printh("=== ATTACK => REPLY ===\n")
