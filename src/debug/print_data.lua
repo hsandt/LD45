929e31ea2636572331dc07ae00b2f7fc7cc4b584
hsandt <hs@fuwar.in>
2019-11-30 01:42:36 +0000

    [DEBUG] print_data shows NPC vulnerabilities and counters

--- src/debug/print_data.lua
+++ src/debug/print_data.lua
@@ -31,25 +31,63 @@ local function get_matching_quotes_for_reply(reply_info)
   return matching_quotes_for_attack
 end
 
+local function can_npc_counter(npc_fighter_info, attack_info)
+  for reply_id in all(npc_fighter_info.initial_reply_ids) do
+    local reply_info = gameplay_data:get_quote(quote_types.reply, reply_id)
+    if gameplay_data:get_quote_match(attack_info, reply_info) then
+      return true
+    end
+  end
+  return false
+end
+
+local function get_attacks_working_against(npc_fighter_info)
+  return filter(gameplay_data.attacks, function (attack_info)
+    return not can_npc_counter(npc_fighter_info, attack_info)
+  end)
+end
+
+local function get_attacks_countered_by(npc_fighter_info)
+  return filter(gameplay_data.attacks, function (attack_info)
+    return can_npc_counter(npc_fighter_info, attack_info)
+  end)
+end
+
 local function print_attack_and_counters_of(attack_id)
   local attack_info = gameplay_data:get_quote(quote_types.attack, attack_id)
-  printh(stringify(attack_info))
-  printh("=>")
+  printh(stringify(attack_info).." =>")
   local matching_quotes_for_attack = get_matching_quotes_for_attack(attack_info)
   for matching_quote_for_attack in all(matching_quotes_for_attack) do
     local reply_info = gameplay_data:get_quote(quote_types.reply, matching_quote_for_attack.reply_id)
-    printh(reply_info.." (power: "..matching_quote_for_attack.power..")")
+    printh("  "..reply_info.." (power: "..matching_quote_for_attack.power..")")
   end
 end
 
 local function print_reply_and_attacks_countered_by(reply_id)
   local reply_info = gameplay_data:get_quote(quote_types.reply, reply_id)
-  printh(stringify(reply_info))
-  printh("=>")
+  printh(stringify(reply_info).." <=")
   local matching_quotes_for_reply = get_matching_quotes_for_reply(reply_info)
   for matching_quote_for_reply in all(matching_quotes_for_reply) do
     local attack_info = gameplay_data:get_quote(quote_types.attack, matching_quote_for_reply.attack_id)
-    printh(attack_info.." (power: "..matching_quote_for_reply.power..")")
+    printh("  "..attack_info.." (power: "..matching_quote_for_reply.power..")")
+  end
+end
+
+local function print_attacks_working_against(npc_fighter_id)
+  local npc_fighter_info = gameplay_data.npc_fighter_info_s[npc_fighter_id]
+  local attacks_working_against = get_attacks_working_against(npc_fighter_info)
+  printh("NPC "..npc_fighter_id.." '"..gameplay_data.npc_info_s[npc_fighter_info.character_info_id].name.."' weak against:")
+  for attack_info in all(attacks_working_against) do
+    printh("  "..attack_info)
+  end
+end
+
+local function print_attacks_countered_by(npc_fighter_id)
+  local npc_fighter_info = gameplay_data.npc_fighter_info_s[npc_fighter_id]
+  local attacks_working_against = get_attacks_countered_by(npc_fighter_info)
+  printh("NPC "..npc_fighter_id.." '"..gameplay_data.npc_info_s[npc_fighter_info.character_info_id].name.."' can counter:")
+  for attack_info in all(attacks_working_against) do
+    printh("  "..attack_info)
   end
 end
 
@@ -64,5 +102,19 @@ printh("=== REPLY <= ATTACK ===\n")
 
 for i = 1, #gameplay_data.replies do
   print_reply_and_attacks_countered_by(i)
+  printh("")
+end
+
+printh("=== NPC VULNERABILITIES ===\n")
+
+for i = 1, #gameplay_data.npc_fighter_info_s do
+  print_attacks_working_against(i)
+  printh("")
+end
+
+printh("=== NPC COUNTERS ===\n")
+
+for i = 1, #gameplay_data.npc_fighter_info_s do
+  print_attacks_countered_by(i)
   printh("")
 end
